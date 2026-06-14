2e9d203ba808b2e4874580a46bc4ddce97c11cfd
PittaShirisha-hub <pshirisha501@gmail.com>
2026-06-14 15:42:21 +0530

    restaurant-reservation-system-part2

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -9,7 +9,9 @@ const sendReservationEmail = async (
 ) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
